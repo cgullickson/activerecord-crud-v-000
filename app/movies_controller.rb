de00679373c350bac5db6d0db078b2661055bd87
Chris Gullickson <cgullickson5@gmail.com>
2018-01-08 22:20:32 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -12,13 +12,20 @@ def can_be_instantiated_and_then_saved
 end
 
 def can_be_created_with_a_hash_of_attributes
+  attributes = {
+      title: "The Sting",
+      release_date: 1973,
+      director: "George Roy Hill",
+      lead: "Paul Newman",
+      in_theaters: false
+  }
   movie = Movie.create(attributes)
 end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = "1990"
+    m.title = "Wayne's World"
+    m.release_date = "1992"
   end
 end
 
